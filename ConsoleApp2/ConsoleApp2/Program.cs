1188e32b1506384673c7cb42ebcb206aec499ab1
khs220507 <khs220507@gmail.com>
2023-12-24 23:12:09 +0900

    oop

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+
+int[] intArray = new int[] { 1, 2, 3, 4, 5 };
+
+IEnumerator enumerator = intArray.GetEnumerator();
+
+while (enumerator.MoveNext()) // 더 이상 열거할 수 없을 때 false를 반환
+{
+    Console.WriteLine(enumerator.Current + ", ");
+}    
---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,24 @@
+﻿class Hardware { }
+
+class USB
+{
+    string name;
+    public USB(string name)
+    {
+        this.name = name;
+    }
+
+    public override string ToString()
+    {
+        return name;
+    }
+}
+
+class Notebook : Hardware, IEnumerable
+{
+    USB[] usbList = new USB[] { new USB("USB1"), new USB("USB2E") };
+    public IEnumerator GetEnumerator() 
+    {
+        return new USBEnumerator(usbList);
+    }
+}
