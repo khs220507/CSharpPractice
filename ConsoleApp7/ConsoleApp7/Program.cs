ddafe587640b13612f65d832e0992a90c512c05b
khs220507 <khs220507@gmail.com>
2023-12-25 21:23:21 +0900

    csharp

---
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,13 @@
+﻿enum Days
+{
+    Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Days today = Days.Sunday;
+        Console.WriteLine(today);
+    }
+}
---
+++ ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,15 @@
+﻿public class Scheduler
+{
+    readonly int second = 1;
+    readonly String name;
+
+    public Scheduler()
+    {
+        this.name = "일정관리"; // 읽기 전용 필드는 생성자에서도 대입 가능
+    }
+
+    public void Run()
+    {
+        this.second = 5; // 컴파일 오류 발생! 일반 메서드에서 값을 대입할 수 없다.
+    }
+}
